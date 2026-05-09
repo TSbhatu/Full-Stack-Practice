9950d4716884701023e5449de2854da1327db44e
Tesfit <tesfaldetsbhatu@gmail.com>
2026-05-09 19:20:26 -0400

    commit controller update

---
+++ SpringBoot_Backend/src/main/java/Spring/com/Backend/SpringBoot_Backend/Controller/Controller.java
@@ -0,0 +1,15 @@
+package Spring.com.Backend.SpringBoot_Backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/health")
+    public String getHealthCheck(){
+        return "YAA I hit my endpoint";
+    }
+}
