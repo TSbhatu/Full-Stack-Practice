ab7b07cec465a495791001cda3691d62b69749db
Tesfit <tesfaldetsbhatu@gmail.com>
2026-05-09 21:54:31 -0400

    forntend added

--- SpringBoot_Backend/src/main/java/Spring/com/Backend/SpringBoot_Backend/Controller/Controller.java
+++ SpringBoot_Backend/src/main/java/Spring/com/Backend/SpringBoot_Backend/Controller/Controller.java
@@ -1,5 +1,6 @@
 package Spring.com.Backend.SpringBoot_Backend.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import java.sql.SQLOutput;
 @RestController
 public class Controller {
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/health")
     public String getHealthCheck(){
         return "YAA I hit my endpoint";
